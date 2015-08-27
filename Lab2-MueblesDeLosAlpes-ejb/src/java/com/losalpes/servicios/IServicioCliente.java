2bb0034da2fac2a9fb0af5552d55e05ecec9c38f
de.vergel10 <de.vergel10@ISC323.sis.virtual.uniandes.edu.co>
2015-08-26 20:55:55 -0500

    commit menu y crud cliente

---
+++ Lab2-MueblesDeLosAlpes-ejb/src/java/com/losalpes/servicios/IServicioCliente.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.losalpes.servicios;
+
+/**
+ *
+ * @author de.vergel10
+ */
+public class IServicioCliente {
+    
+}
